7e475369c9f2160f4afae3fac0c1599ecfa83696
Srinivas Gorur-Shandilya <code@srinivas.gs>
2022-09-11 10:26:42 -0400

    switched to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
 """setup file"""
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="pyvocab",
-    version="22.7.20",
-    packages=find_packages(exclude=("tests", "docs")),
-    description="simple vocabulary builder in python",
-    url="https://github.com/sg-s/pyvocab",
-    author="Srinivas Gorur-Shandilya",
-    author_email="code@srinivas.gs",
-    install_requires=[
-        "pandas>=1.3.2",
-        "watchdog",
-        "streamlit>=1.11.0",
-        "interrogate>=1.0",
-        "python-Levenshtein",
-        "pycore @ git+https://github.com/sg-s/pycore.git",
-    ],
-    extras_require={
-        "dev": [
-            "pytest",
-        ]
-    },
-)
+setup()
